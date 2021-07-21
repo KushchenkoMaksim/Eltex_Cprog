129428ddfccae2abf3d612cf00cbd656bb912d0f
Maxim Kushchenko <kushchenko2001@outlook.com>
2021-07-21 20:47:37 +0700

    added comments to lab1/Task2

--- Lab1/Task2.c
+++ Lab1/Task2.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 //Динамически выделяет память под строку, и считывает в неё с клавиатуры
-char * readString(int *size) {
+char * readString() {
 	int strSize = 10;
 	int delta = 10;
 	char *str;
@@ -28,8 +28,6 @@ char * readString(int *size) {
 	str[i-1] = '\0';
 	if ( !(str = (char *)realloc(str, i * sizeof(char))) )
 		return 0;
-	if(size)
-		*size = i;
 	return str;
 }
 
@@ -50,38 +48,34 @@ char * findDeepestBrackets(char *str) {
 			--level;
 		}
 	}
-	if(deepest)
-		printf("find level %d it is %s\n", maxLevel, deepest +1);
-	else
-		printf("find nothing\n");
 	return deepest;
 }
 
 int main() {
 	char *strIn;
 	char *strOut;
-	int size;
 
-	//получаем строку для работы
-	if ( !(strIn = readString(&size)) ) {
+	//получаем входную строку
+	if ( !(strIn = readString()) ) {
 		printf("Cant read string");
 		exit(1);
 	}
 
-	if ( !(strOut = (char *)malloc(size * sizeof(char))) ) {
+	//создаем выходную строку
+	if ( !(strOut = (char *)malloc(strlen(strIn) * sizeof(char))) ) {
 		exit(1);
 	}
 	strOut[0] = '\0';
 
+	//Из функции получаем указатель на самую внутреннюю пару скобок на данный момент.
 	for (char *deepest = findDeepestBrackets(strIn); deepest != 0; deepest = findDeepestBrackets(strIn) ) {
 		int i;
+		//Считаем длину фрагмента и переносим из входной строки в выходную
 		for (i = 0; deepest[i] != '}'; ++i);
-		printf("Strin %s\n", strIn);
 		strncat(strOut, deepest + 1, i - 1);
 		strcpy(deepest, deepest + i + 1);
-		printf("Strin %s\n", strIn);
-		printf("Strout %s\n", strOut);
 	}
+	//Текст без скобок тоже переносим в выходную строку
 	strcat(strOut, strIn);
 	printf("Result %s\n", strOut);
 	free(strIn);
